778341b4288e728415692a20b4750a9830455ad0
vivek <eegavivekreddy@gmail.com>
2024-03-07 12:01:09 -0500

    Step-6 completed removed extends but testcases failed as we are using isEmpty() in it.

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,11 +3,11 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E> {
+public class MyStack<E>  {
 
     private ArrayList<E> delegate;
     public MyStack() {
-        delegate = this;
+        delegate = new ArrayList<>();
     }
 
     public void push(E e) {
@@ -16,7 +16,7 @@ public class MyStack<E> extends ArrayList<E> {
 
     public E pop() {
         if (delegate.isEmpty()) throw new EmptyStackException();
-        E e = delegate.get(size() - 1);
+        E e = delegate.get(delegate.size() - 1);
         delegate.remove(delegate.size() -1 );
         return e;
     }
